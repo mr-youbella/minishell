a3d24c135ca951e06a6ddf7f8e395285bcad9463
Wkannouf <wkannouf@student.42.fr>
2025-06-16 20:38:34 +0100

    c

--- split_first_cmd.c
+++ split_first_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_first_cmd.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youbella <youbella@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 22:44:16 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/06/14 22:13:05 by youbella         ###   ########.fr       */
+/*   Updated: 2025/06/16 20:36:59 by wkannouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,6 +89,19 @@ static char	*extract_word(const char *str, char c, size_t index)
 			printf(BLUE"minishell%s: syntax %serror%s: unclosed quote.\n", DEF, RED, DEF);
 			return (NULL);
 		}
+	}
+	if (str[index + i] != '"' && str[index + i])
+	{
+		index++;
+		while (str[index + i] && str[index + i] != '"')
+			i++;
+		if (str[index + i] && str[index + i] == '"')
+		{
+			printf(BLUE"minishell%s: syntax %serror%s: unclosed quote.\n", DEF, RED, DEF);
+			return (NULL);
+		}
+		else
+			return (ft_substr(str, index, i));
 	}
 	else if (str[index + i] == 39)
 	{
