40f551991d93ea641d2072d911bea24ff401113c
Younes Oubellal <younesoubllal@gmail.com>
2025-06-17 23:51:24 +0100

    Push

--- split_first_cmd.c
+++ split_first_cmd.c
@@ -6,7 +6,7 @@
 /*   By: youbella <youbella@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 22:44:16 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/06/16 22:59:56 by youbella         ###   ########.fr       */
+/*   Updated: 2025/06/17 23:51:13 by youbella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,149 +17,86 @@ static size_t	count_words(const char *str, char c)
 	size_t	i;
 	size_t	words;
 	short	is_word;
+	short	is_double_quote;
+	short	is_single_quote;
 
-	i = 0;
-	words = 0;
-	is_word = 0;
-	while (str[i] && str[i] != '|')
+	(1) && (words = 0, is_word = 0, i = 0);
+	is_double_quote = 0;
+	is_single_quote = 0;
+	while (str[i])
 	{
-		if (str[i] == '"')
-		{
-			words++;
-			is_word = 1;
-			i++;
-			while (str[i] && str[i] != '"')
-				i++;
-			if (str[i] == '"')
-				i++;
-			else
-			{
-				if (!str[i])
-				{
-					words--;
-					is_word = 0;
-					break ;
-				}
-			}
-		}
-		else if (str[i] == 39)
-		{
-			words++;
-			is_word = 1;
-			i++;
-			while (str[i] && str[i] != 39)
-				i++;
-			if (str[i] == 39)
-				i++;
-			else
-			{
-				if (!str[i])
-				{
-					words--;
-					is_word = 0;
-					break ;
-				}
-			}
-		}
-		if (str[i] != c && !is_word)
-		{
-			words++;
-			is_word = 1;
-		}
-		else if (str[i] == c)
+		if (str[i] == 39 && !is_double_quote)
+			is_single_quote = !is_single_quote;
+		else if(str[i] == '"' && !is_single_quote)
+			is_double_quote = !is_double_quote;
+		else if (str[i] == c && !is_double_quote && !is_single_quote)
 			is_word = 0;
+		else if (!is_word)
+		{
+			words++;
+			is_word = 1;
+		}
 		i++;
 	}
 	return (words);
 }
 
-static char	*extract_word(const char *str, char c, size_t index)
+static char	*extract_word(const char *str, char c)
 {
-	size_t	i;
+	size_t	len_str;
+	size_t	quotes;
+	char	*p;
 	
-	i = 0;
-	if (str[index + i] != '"'  && str[index + i] != 39 && str[index + i])
-	{
-		size_t j = 0;
-		while(str[index + j] != ' ' && str[index + j] != '|' && str[index + j])
-		{
-			if (str[index + j] == '"' || str[index + j] == 39)
-			{
-				printf(BLUE"minishell%s: syntax %serror%s: unclosed quote.\n", DEF, RED, DEF);
-				return (NULL);
-			}
-			j++;
-		}
-	}
-	else if (str[index + i] == '"' && str[index + i])
-	{
-		index++;
-		c = '"';
-		while (str[index + i] && str[index + i] != c)
-				i++;
-		if (!str[index + i])
-		{
-			printf(BLUE"minishell%s: syntax %serror%s: unclosed quote.\n", DEF, RED, DEF);
-			return (NULL);
-		}
-	}
-	else if (str[index + i] == 39 && str[index + i])
-	{
-		index++;
-		c = 39;
-		while (str[index + i] && str[index + i] != c)
-				i++;
-		if (!str[index + i])
-		{
-			printf(BLUE"minishell%s: syntax %serror%s: unclosed quote.\n", DEF, RED, DEF);
-			return (NULL);
-		}
-	}
-	while (str[index + i] && str[index + i] != c && str[index + i] != '|')
-		i++;
-	return (ft_substr(str, index, i));
-}
-
-static short	ft_free(char **p, size_t j)
-{
-	if (!p[j - 1])
-	{
-		while (j--)
-			free(p[j]);
-		free(p);
-		return (0);
-	}
-	return (1);
+	len_str = 0;
+	if (str)
+		len_str = ft_strlen(str);
+	p = ft_calloc(len_str + 2, 1);
+	if (!p)
+		return (NULL);
+	if (str)
+		ft_strlcpy(p, str, len_str + 2);
+	p[len_str] = c;
+	return (p);
 }
 
 char	**ft_split_first_cmd(char const *s, char c)
 {
+	char	**p;
+	char	*buffer;
 	size_t	i;
 	size_t	j;
-	char	**p;
-
-	if (!s)
-		return (NULL);
+	short	is_single_quote;
+	short	is_double_quote;
+	
 	i = 0;
 	j = 0;
-	p = ft_calloc(count_words(s, c) + 1, 8);
+	is_double_quote = 0;
+	is_single_quote = 0;
+	buffer = NULL;
+	printf ("%zu\n", count_words(s, c));
+	p =	ft_calloc(count_words(s, c) + 1, sizeof(char *));
 	if (!p)
 		return (NULL);
-	while (s[i] && s[i] != '|')
+	while (s[i])
 	{
-		if (s[i] != c)
+		if (s[i] == 39 && !is_double_quote)
+			is_single_quote = !is_single_quote;
+		else if(s[i] == '"' && !is_single_quote)
+			is_double_quote = !is_double_quote;
+		else if (s[i] == c && !is_double_quote && !is_single_quote)
 		{
-			p[j++] = extract_word(s, c, i);
-			if (!ft_free(p, j))
-				return (NULL);
-			if (s[i] == '"')
-				i += 2;
-			if (s[i] == 39)
-				i += 2;
-			i += ft_strlen(p[j - 1]);
+			if (buffer)
+			{
+				p[j] = buffer;
+				buffer = NULL;
+				j++;
+			}
 		}
 		else
-			i++;
+			buffer = extract_word(buffer, s[i]);
+		i++;		
 	}
+	if (buffer)
+		p[j] = buffer;
 	return (p);
 }
