bd79af678a9c6ebf51618875340307ca8723530d
Younes Oubellal <108194849+mr-youbella@users.noreply.github.com>
2025-06-21 21:02:02 +0100

    Update search.c

--- search.c
+++ search.c
@@ -3,26 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   search.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youbella <youbella@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 21:18:19 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/06/18 21:43:52 by wkannouf         ###   ########.fr       */
+/*   Updated: 2025/06/19 22:46:08 by youbella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-short	is_unset(char *export, char **unset)
+t_list	*search_in_list(char *str, t_list *list)
 {
-	size_t	i;
-	
-	i = 0;
-	while (unset[i])
-	{
-		if (!ft_strncmp(export, unset[i], ULONG_MAX))
-			return (1);
-		i++;
-	}
-	return (0);
+    while (list)
+    {
+        if (ft_strlen(str) == ft_strlen((char *)list->content) && !ft_strncmp(str, (char *)list->content, ft_strlen(str)))
+            return (list);
+        list = list->next;
+    }
+    return (NULL);
 }
-
