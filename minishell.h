2379c403bcab0fdd1ad9a8c231f369d0a1bae744
Younes Oubellal <108194849+mr-youbella@users.noreply.github.com>
2025-06-21 21:03:46 +0100

    Update minishell.h

--- minishell.h
+++ minishell.h
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youbella <youbella@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 06:03:27 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/06/18 20:46:28 by wkannouf         ###   ########.fr       */
+/*   Updated: 2025/06/19 13:56:13 by youbella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "libft/libft.h"
+# include <stdio.h>
+# include <signal.h>
+# include <termios.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "libft/libft.h"
 
 # define GREEN "\033[32m"
 # define RED "\033[31m"
@@ -26,5 +29,6 @@
 # define DEF "\033[0m"
 
 char	**ft_split_first_cmd(char const *s, char c, int status);
+t_list	*search_in_list(char *str, t_list *list);
 
 #endif
