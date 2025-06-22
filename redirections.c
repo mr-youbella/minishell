4c6df79f2f17787334bb965d462d3d1cac39cd4f
Wkannouf <wkannouf@student.42.fr>
2025-06-22 18:08:16 +0100

    x

--- redirections.c
+++ redirections.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   directions.c                                       :+:      :+:    :+:   */
+/*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 15:23:33 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/06/22 17:59:57 by wkannouf         ###   ########.fr       */
+/*   Updated: 2025/06/22 18:08:11 by wkannouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,8 @@ t_redirect	*search_directions_name_file(char *str, t_redirect *redirect)
 			while (str[i] == ' ')
 				i++;
 			start = i;
-			if (ft_strncmp(redirect->type_redirection, ">>", 2) || ft_strncmp(redirect->type_redirection, ">", 1))
-			{
-				while (str[i] && str[i] != '>' && str[i] != ' ')
-					i++;
-			}
+			while (str[i] && str[i] != '>' && str[i] != ' ')
+				i++;
 			len = i - start;
 			redirect->file_name = malloc(len + 1);
 			ft_strlcpy(redirect->file_name, &str[start], len + 1);
