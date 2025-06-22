3853c789f44e1bead63011ee7091771d94d9ab38
Wkannouf <wkannouf@student.42.fr>
2025-06-22 19:16:04 +0100

    x

--- redirections.c
+++ redirections.c
@@ -6,34 +6,91 @@
 /*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 15:23:33 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/06/22 18:08:11 by wkannouf         ###   ########.fr       */
+/*   Updated: 2025/06/22 19:15:51 by wkannouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_redirect	*search_directions_name_file(char *str, t_redirect *redirect)
+typedef struct s_redirect
 {
-	size_t	i;
-	size_t	len;
-	size_t	start;
+	char	*type_redirection;
+	char	*file_name;
+	struct s_redirect	*next;
+}	t_redirect;
+
+t_redirect	*lstlast(t_redirect *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	lstadd_back(t_redirect **lst, t_redirect *new)
+{
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	lstlast(*lst)->next = new;
+}
+
+
+t_redirect	*lstnew(char *file, char *redirection)
+{
+	t_redirect	*node;
+
+	node = malloc(sizeof(t_redirect));
+	if (!node)
+		return (NULL);
+	node->type_redirection = redirection;
+	node->file_name = file;
+	node->next = NULL;
+	return (node);
+}
+
+t_redirect	*search_directions_name_file(char *str)
+{
+	size_t		i;
+	size_t		len;
+	size_t		start;
+	t_redirect	*red;
+	t_redirect	*new_node;
+	char		*re;
+	char		*file;
 
 	start = 0;
 	len = 0;
 	i = 0;
-	while(redirect && str[i])
+	red = NULL;
+	while(str[i])
 	{
 		if (str[i] == '>')
 		{
 			if (str[i + 1] == '>')
 			{
-				redirect->type_redirection = ">>";
 				i += 2;
+				if (str[i] == 0)
+				{
+					printf("minishell: Syntax error.\n");
+					return (NULL);
+				}
+				re = ft_strdup(">>");
 			}
 			else
 			{
-				redirect->type_redirection = ">";
 				i++;
+				if (str[i] == 0)
+				{
+					printf("minishell: Syntax error.\n");
+					return (NULL);
+				}
+				re = ft_strdup(">");
 			}
 			while (str[i] == ' ')
 				i++;
@@ -41,14 +98,13 @@ t_redirect	*search_directions_name_file(char *str, t_redirect *redirect)
 			while (str[i] && str[i] != '>' && str[i] != ' ')
 				i++;
 			len = i - start;
-			redirect->file_name = malloc(len + 1);
-			ft_strlcpy(redirect->file_name, &str[start], len + 1);
-			redirect = redirect->next;
+			file = ft_substr(str, start, len);
+			new_node = lstnew(file, re);
+			lstadd_back(&red, new_node);
 		}
 		else 
 			i++;
 	}
-	
-	
-	return (NULL);
+	return (red);
 }
+
