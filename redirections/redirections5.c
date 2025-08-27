feac718244dfc9d0bb508a4fb2c0d649c906f7e4
Younes Oubellal <younesoubllal@gmail.com>
2025-08-27 23:29:25 +0100

    fix

--- redirections/redirections5.c
+++ redirections/redirections5.c
@@ -5,73 +5,123 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: youbella <youbella@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/27 12:57:09 by youbella          #+#    #+#             */
-/*   Updated: 2025/08/27 23:27:47 by youbella         ###   ########.fr       */
+/*   Created: 2025/08/26 03:18:01 by youbella          #+#    #+#             */
+/*   Updated: 2025/08/27 23:29:13 by youbella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static char	*defualt_env(char *str, t_var *variables)
-{
-	t_list	*new_leak;
-	char	*new_env;
-
-	new_env = ft_strdup(str);
-	new_leak = ft_lstnew(new_env);
-	ft_lstadd_back(&variables->leaks, new_leak);
-	return (new_env);
-}
-
-char	**add_env(char **env, t_var *variables)
-{
-	char	*pwd;
-	char	**new_env;
-
-	if (!env[0])
-	{
-		pwd = getcwd(NULL, 0);
-		new_env = malloc(6 * sizeof(char *));
-		if (!new_env)
-			return (NULL);
-		new_env[0] = defualt_env("OLDPWD", variables);
-		new_env[1] = defualt_env
-			("PATH=/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.", variables);
-		new_env[2] = defualt_env(ft_strjoin("PWD=", pwd), variables);
-		new_env[3] = defualt_env("SHLVL=2", variables);
-		new_env[4] = defualt_env("_=/usr/bin/env", variables);
-		new_env[5] = NULL;
-		free(pwd);
-		return (new_env);
-	}
-	return (NULL);
-}
-
-void	change_v_export(char *v_export, char *name_var_export,
-							t_var *variables, t_env *env_var)
+short	is_exist_redirect_pipe(char *cmd_line, char redirect_pipe)
 {
 	size_t	i;
-	t_list	*new_leak;
-	char	*value;
-	char	*tmp;
+	char	quot;
 
 	i = 0;
-	while (v_export && v_export[i] && v_export[i] != '=')
-		i++;
-	if (v_export && variables->is_append_val)
+	while (cmd_line[i])
 	{
-		env_var->var_export = ft_strjoin(name_var_export, "=");
-		tmp = env_var->var_export;
-		env_var->var_export = ft_strjoin(env_var->var_export,
-				ft_getenv(name_var_export, variables));
-		tmp = env_var->var_export;
-		free(tmp);
-		value = ft_substr(v_export, i + 1, ft_strlen(v_export) - i);
-		env_var->var_export = ft_strjoin(env_var->var_export, value);
-		free(tmp);
-		free(value);
+		if (cmd_line[i] == '"' || cmd_line[i] == 39)
+		{
+			quot = cmd_line[i];
+			i++;
+			while (cmd_line[i] && cmd_line[i] != quot)
+				i++;
+			if (cmd_line[i] == quot)
+				i++;
+		}
+		if (!cmd_line[i])
+			break ;
+		if ((redirect_pipe == 'r' && cmd_line[i] == '>')
+			|| (redirect_pipe == 'r' && cmd_line[i] == '<')
+			|| (redirect_pipe == '|' && cmd_line[i] == '|'))
+			return (1);
+		i++;
 	}
-	variables->is_append_val = 0;
-	new_leak = ft_lstnew(name_var_export);
-	ft_lstadd_back(&variables->leaks, new_leak);
+	return (0);
+}
+
+static short	check_ambiguous_dollar(char *str, t_var *variables,
+										t_ft_var *var)
+{
+	size_t	start;
+	size_t	var_len;
+	size_t	j;
+	char	*var_name;
+	char	*var_value;
+
+	var_value = NULL;
+	if (str[var->j] == '$')
+	{
+		1 && (j = 0, var_len = 0, start = var->j, var->j++);
+		while ((str[var->j]) && (ft_isalnum(str[var->j]) || str[var->j] == '_'))
+			1 && (var_len++, var->j++);
+		var_name = ft_substr(str, start, var_len + 1);
+		var_value = ft_dollar(var_name, variables, 0);
+		free(var_name);
+		while (var_value[j])
+		{
+			if (var_value[j] == ' ')
+				return (free(var_value), 1);
+			j++;
+		}
+	}
+	else
+		var->j++;
+	return (free(var_value), 0);
+}
+
+short	is_ambiguous_redirect(char *token, t_var *variables)
+{
+	t_ft_var	*var;
+	char		quot;
+
+	var = malloc(sizeof(t_ft_var));
+	if (!var)
+		return (ft_status(1, 1), 0);
+	ft_memset(var, 0, sizeof(t_ft_var));
+	var->j = 0;
+	while (token[var->j])
+	{
+		if (token[var->j] == 39 || token[var->j] == '"')
+		{
+			quot = token[var->j];
+			var->j++;
+			while (token[var->j] && token[var->j] != quot)
+				var->j++;
+			if (token[var->j] == quot)
+				var->j++;
+		}
+		if (!token[var->j])
+			break ;
+		if (check_ambiguous_dollar(token, variables, var))
+			return (free(var), 1);
+	}
+	return (free(var), 0);
+}
+
+short	check_syntax_redirect(char **tokens, t_ft_var *list_var)
+{
+	if ((ft_strlen(tokens[list_var->j]) == 2
+			&& ((!ft_strncmp(tokens[list_var->j], ">>", 2))
+				|| (!ft_strncmp(tokens[list_var->j], "<<", 2))))
+		|| ((ft_strlen(tokens[list_var->j]) == 1)
+			&& ((!ft_strncmp(tokens[list_var->j], ">", 1))
+				|| (!ft_strncmp(tokens[list_var->j], "<", 1)))))
+	{
+		list_var->j++;
+		if ((!tokens[list_var->j] || (ft_strlen(tokens[list_var->j]) == 2
+					&& !ft_strncmp(tokens[list_var->j], ">>", 2))
+				|| !ft_strncmp(tokens[list_var->j], "<<", 2))
+			|| (!tokens[list_var->j] || (ft_strlen(tokens[list_var->j]) == 1
+					&& (!ft_strncmp(tokens[list_var->j], ">", 1)
+						|| !ft_strncmp(tokens[list_var->j], "<", 1)))))
+		{
+			ft_putstr_fd("\033[34mminishell:\033[31m ", 2);
+			ft_putstr_fd("syntax error in redirection.\n\033[0m", 2);
+			return (ft_status(258, 1), -1);
+		}
+	}
+	else
+		return (list_var->j++, 0);
+	return (1);
 }
